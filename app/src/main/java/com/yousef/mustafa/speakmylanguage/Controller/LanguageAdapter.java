4074ed910b736a040fb10fc9ae9c3bc2b914e321
Mustafa Yousef <mustafa.yousef@cologne-intelligence.de>
2017-11-27 22:52:56 +0100

    added a dialog

--- app/src/main/java/com/yousef/mustafa/speakmylanguage/Controller/LanguageAdapter.java
+++ app/src/main/java/com/yousef/mustafa/speakmylanguage/Controller/LanguageAdapter.java
@@ -7,12 +7,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import com.yousef.mustafa.speakmylanguage.Model.Language;
 import com.yousef.mustafa.speakmylanguage.R;
-import com.yousef.mustafa.speakmylanguage.View.AddNewLanguageActivity;
 
 import java.util.ArrayList;
 
